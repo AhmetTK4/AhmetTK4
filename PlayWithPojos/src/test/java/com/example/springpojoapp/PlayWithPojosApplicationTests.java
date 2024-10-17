e6d98584387fe437938de3cfe066b4b9ae29287f
Ahmet <AhmetTK4@users.noreply.github.com>
2024-10-17 21:37:00 +0300

    Properties added

---
+++ PlayWithPojos/src/test/java/com/example/springpojoapp/PlayWithPojosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpojoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayWithPojosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
