79deb417196fd077b0cc5a487e6d0757eab790cf
Ahmet <AhmetTK4@users.noreply.github.com>
2024-10-18 01:19:02 +0300

    Dockerize app

--- PlayWithPojos/src/main/java/com/example/springpojoapp/repository/UserRepository.java
+++ PlayWithPojos/src/main/java/com/example/springpojoapp/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package com.example.springpojoapp.repository;
 
-import com.example.springpojoapp.model.User;
+import com.example.springpojoapp.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
+public interface UserRepository extends JpaRepository<AppUser, Long> {
+    Optional<AppUser> findByUsername(String username);
 }
